b23ee3f5bb72669cecb3a93dcf3f9a3d438876ab
Arisodee <arisodee@gmail.com>
2021-02-03 00:11:58 +0300

    add view functions for comments

--- dev/views.py
+++ dev/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from dev.models import Post, Comment
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from dev.forms import PostForm, CommentForm     
-from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView)
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 
 # Create your views here.
 
@@ -15,7 +17,7 @@ class PostListView(ListView):
     model = Post 
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -31,6 +33,7 @@ class CreatePostView(LoginRequiredMixin,CreateView):
     model = Post
 
 
+
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     redirect_field_name = 'dev/post_detail.html'
@@ -40,9 +43,59 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
     model = Post
 
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+
+    model = Post
+
+    success_url = reverse_lazy('post_list')
+     
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'dev/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull= True).order_by('created_date')
 
 
+##comments views
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
 
 
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post.pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            cmment.save()
+            return redirect('post_detail', pk=post.pk)
 
+    else:
+        form = CommentForm()
+
+    return render (request, 'dev/comment_form.html',{'form':form})
+        
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=poat_pk)
      
